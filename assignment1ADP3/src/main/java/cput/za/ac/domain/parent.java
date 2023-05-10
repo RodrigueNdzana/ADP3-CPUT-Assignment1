a0694fa1600e8779f69e7f707a93ff33b40ffbc7
Mxolisi Ndovela <yanela692@gmail.com>
2023-05-10 10:59:02 +0300

    second commit

--- assignment1ADP3/src/main/java/cput/za/ac/domain/parent.java
+++ assignment1ADP3/src/main/java/cput/za/ac/domain/parent.java
@@ -1,2 +1,53 @@
-package cput.za.ac.domain;public class parent {
+package cput.za.ac.domain;
+
+public class parent {
+    private int parent_id;
+    private String parent_name;
+    private String phone_number;
+    private String Address;
+    private String email;
+    private int number;
+
+    public static class Builder {
+        private int parent_id;
+        private String parent_name;
+        private String phone_number;
+
+        public Builder parentId(int value) {
+            parent_id = value;
+            return this;
+        }
+
+        public Builder parentName(String value) {
+            parent_name = value;
+            return this;
+        }
+
+        public Builder phoneNumber(String value) {
+            phone_number = value;
+            return this;
+        }
+
+        public parent build() {
+            return new parent(this);
+        }
+    }
+
+    private parent(Builder builder) {
+        parent_id = builder.parent_id;
+        parent_name = builder.parent_name;
+        phone_number = builder.phone_number;
+    }
+
+    public int getParentId() {
+        return parent_id;
+    }
+
+    public String getParentName() {
+        return parent_name;
+    }
+
+    public String getPhoneNumber() {
+        return phone_number;
+    }
 }
--- assignment1ADP3/src/main/java/cput/za/ac/factory/subjectFactory.java
+++ assignment1ADP3/src/main/java/cput/za/ac/factory/subjectFactory.java
@@ -1,2 +1,13 @@
-package cput.za.ac.factory;public class subjectFactory {
+package cput.za.ac.factory;
+
+import cput.za.ac.domain.subject;
+public class subjectFactory {
+    public static subject createSubject(String subject_code, String subject_name) {
+        return new subject.Builder()
+                .subjectCode(subject_code)
+                .subjectName(subject_name)
+                .build();
+
+    }
+    subject subject = subjectFactory.createSubject("ADP360S", "Applications Development");
 }
